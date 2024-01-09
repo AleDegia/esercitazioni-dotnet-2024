b42d5ec9f7d6f4338c5fff1c3bb76b27299e1691
aledegia <degia.ale@gmail.com>
2024-01-09 10:15:17 +0100

    program con namespace

--- esercitazione1/Program.cs
+++ esercitazione1/Program.cs
@@ -1,7 +1,10 @@
-﻿class Program
+﻿namespace esercitazioni //se un file non ha questo namespace non ci potrò richamare la class Program
+{
+    class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
         }
     }
+} 
