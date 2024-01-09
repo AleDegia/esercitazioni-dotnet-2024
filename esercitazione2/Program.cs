62a2e04daf21a3ba4653053558190e8f2ce0ed96
aledegia <degia.ale@gmail.com>
2024-01-09 12:14:56 +0100

    dichiarare una variabile di tipo stringa

---
+++ esercitazione2/Program.cs
@@ -0,0 +1,11 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        string nome = "Alessandro";
+        string cognome = "Deg";
+        Console.WriteLine("Ciao " + nome); //concatenazione
+        Console.WriteLine($"Ciao {nome}"); //interpolazione di stringhe ( col + è concatenazione)
+        Console.WriteLine($"Ciao {nome} {cognome}");
+    }
+}
