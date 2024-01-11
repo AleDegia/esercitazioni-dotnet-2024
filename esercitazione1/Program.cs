fe72f658f9d988678f552aca71a4bf636a199a99
aledegia <degia.ale@gmail.com>
2024-01-11 10:50:24 +0100

    liste con numeri

--- esercitazione1/Program.cs
+++ esercitazione1/Program.cs
@@ -1,12 +1,13 @@
 ﻿class Program{
     static void Main(string[] args)
     {
-        List <string> nomi = new List<string>(); 
-        nomi.Add("Mario");
-        nomi.Add("Luigi"); 
-        nomi.Add("Giovanni"); 
+        List <int> numeri = new List<int>(); //gli int non necessitano gli apici doppi nemmeno qui
+        numeri.Add(10);
+        numeri.Add(20); 
+        numeri.Add(30); 
         
-        Console.WriteLine($"Ciao {nomi[0]}, {nomi[1]} e {nomi[2]}");
+        Console.WriteLine($" {numeri[0]}, {numeri[1]} e {numeri[2]}");
+        Console.WriteLine($" {numeri}"); //da la posizione dove ha locato in memoria l'oggetto
     }
 }
 
--- esercitazione1/README.md
+++ esercitazione1/README.md
@@ -211,3 +211,20 @@ class Program{
     }
 }
 ```
+
+### 11.1 - Liste con numeri
+
+```c#
+class Program{
+    static void Main(string[] args)
+    {
+        List <int> numeri = new List<int>(); //gli int non necessitano gli apici doppi nemmeno qui
+        numeri.Add(10);
+        numeri.Add(20); 
+        numeri.Add(30); 
+        
+        Console.WriteLine($" {numeri[0]}, {numeri[1]} e {numeri[2]}");
+        Console.WriteLine($" {numeri}"); //da la posizione dove ha locato in memoria l'oggetto
+    }
+}
+```
