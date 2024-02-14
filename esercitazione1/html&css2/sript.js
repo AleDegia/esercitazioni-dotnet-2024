c1fb8b55a64b4ef6d84b0595bb64872225fbbc3a
aledegia <degia.ale@gmail.com>
2024-02-14 16:02:31 +0100

    esercizio sito

---
+++ esercitazione1/html&css2/sript.js
@@ -0,0 +1,17 @@
+// document.querySelector('.menu-toggle').addEventListener('click', function() {
+//     document.querySelector('nav ul').classList.toggle('show');
+// });
+
+document.querySelector('.registrati').addEventListener('click', function() {
+    // Seleziona l'elemento della scritta da modificare
+    var titolo = document.querySelector('.logo');
+    
+    // Modifica la scritta
+    titolo.textContent = "Nuovo Titolo";
+});
+
+
+
+
+
+
