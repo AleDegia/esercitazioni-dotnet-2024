4f3ecaf931e5db59dc4e01a7e18a8b56369853d5
aledegia <degia.ale@gmail.com>
2024-02-27 14:48:16 +0100

    oop costruttore

--- esercitazione1/OOP/Persona.cs
+++ esercitazione1/OOP/Persona.cs
@@ -3,4 +3,18 @@ class Persona
     public string nome;
     public string cognome;
     public int eta;
+
+    public Persona(string nome, string cognome, int eta) 
+    {
+        this.nome = nome;           //this si riferisce all'oggetto creato col new, con cui chiamo il costruttore
+        this.cognome = cognome;
+        this.eta = eta;
+    }
+
+    public void Stampa()
+    {
+        System.Console.WriteLine("Nome: " + nome);
+        System.Console.WriteLine("Cognome: " + cognome);
+        System.Console.WriteLine("Eta: " + eta);
+    }
 }
--- esercitazione1/OOP/Program.cs
+++ esercitazione1/OOP/Program.cs
@@ -2,19 +2,16 @@
 {
     static void Main(string[] args)
     {
-        Persona p = new Persona();
-        p.nome = "Mario";
-        p.cognome = "Rossi";
-        p.eta = 30;
+        Persona p = new Persona("Mario", "Cognome", 30);
+        Persona p2 = new Persona("Luca", "Verdi", 24);
+        p.Stampa();
+        p2.Stampa();
 
-        Persona p2 = new Persona();
-        p2.nome = "Luca";
-        p2.cognome = "verdi";
-        p2.eta = 24;
+        //questi cw qui sotto li userei se non avessi il metodo
         
-        System.Console.WriteLine("Nome: " + p.nome);
-        System.Console.WriteLine("Cognome: " + p.cognome);
-        System.Console.WriteLine("Eta: " + p.eta);
-        System.Console.WriteLine("Nome persona 2: " + p2.nome);
+        // System.Console.WriteLine("Nome: " + p.nome);
+        // System.Console.WriteLine("Cognome: " + p.cognome);
+        // System.Console.WriteLine("Eta: " + p.eta);
+        // System.Console.WriteLine("Nome persona 2: " + p2.nome);
     }
 }
