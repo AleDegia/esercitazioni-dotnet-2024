1032099d5784d01d30a3749495d38d83bd519c8f
aledegia <degia.ale@gmail.com>
2024-02-27 14:34:33 +0100

    oop

---
+++ esercitazione1/OOP/Program.cs
@@ -0,0 +1,20 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Persona p = new Persona();
+        p.nome = "Mario";
+        p.cognome = "Rossi";
+        p.eta = 30;
+
+        Persona p2 = new Persona();
+        p2.nome = "Luca";
+        p2.cognome = "verdi";
+        p2.eta = 24;
+        
+        System.Console.WriteLine("Nome: " + p.nome);
+        System.Console.WriteLine("Cognome: " + p.cognome);
+        System.Console.WriteLine("Eta: " + p.eta);
+        System.Console.WriteLine("Nome persona 2: " + p2.nome);
+    }
+}
